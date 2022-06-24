577a638fa8bf9b98e4c90506a7709b3a1b2d7b9a
Kalesanwo <kalesanwoyinka99@gmail.com>
2022-06-24 12:26:21 +0100

    simple alert message

---
+++ alert.js
@@ -0,0 +1,7 @@
+const myHeight='My height is 174.5cm';
+const myName='My name is Olayinka Kalesanwo';
+const myCountry='My Country is Nigeria';
+
+alert(myName);
+alert(myCountry);
+alert(myHeight);
